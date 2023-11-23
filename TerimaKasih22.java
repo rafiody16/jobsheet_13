c1186720f631c99fe21fe89c577d560c6380061e
rafiody16 <rafiody16@gmail.com>
2023-11-23 20:18:55 +0700

    Percobaan 1

---
+++ TerimaKasih22.java
@@ -0,0 +1,14 @@
+/**
+ * TerimaKasih22
+ */
+public class TerimaKasih22 {
+    public static void ucapanTerimaKasih ()
+    {
+        System.out.println("Thank you for being the best teacher in the world.\n"+
+        "You inspired in me a love  for learning and made me feel like i could ask you anything.");
+    }
+
+    public static void main(String[] args) {
+        ucapanTerimaKasih();
+    }
+}
