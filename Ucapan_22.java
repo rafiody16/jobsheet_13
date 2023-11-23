d0e716b03ac948be905f2649797f4a0223ca023a
rafiody16 <rafiody16@gmail.com>
2023-11-23 21:15:11 +0700

    Percobaan 3

--- Ucapan_22.java
+++ Ucapan_22.java
@@ -1,17 +1,16 @@
 import java.util.Scanner;
 
 public class Ucapan_22 {
-    public static String PenerimaUcapan()
+    public static void PenerimaUcapan()
     {
         Scanner sc = new Scanner(System.in);
         System.out.println("Tuliskan Nama orang yang ingin anda beri ucapan: ");
         String namaOrang = sc.nextLine();
         sc.close();
-        return namaOrang;
+        System.out.println("Thank you "+ namaOrang +"\nMay the force be with you.");
     }
 
     public static void main(String[] args) {
-        String nama = PenerimaUcapan();
-        System.out.println("Thank you "+ nama +"\nMay the force be with you.");
+        PenerimaUcapan();
     }
 }
