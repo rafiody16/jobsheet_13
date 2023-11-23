db45710fe71cb40098397655602bc20e737e3a62
rafiody16 <rafiody16@gmail.com>
2023-11-23 21:26:15 +0700

    Percobaan 4

---
+++ UcapanTerimaKasih_22.java
@@ -0,0 +1,23 @@
+import java.util.Scanner;
+
+public class UcapanTerimaKasih_22 {
+    public static String PenerimaUcapan()
+    {
+        Scanner sc = new Scanner(System.in);
+        System.out.println("Tuliskan NAMA orang yang ingin anda beri ucapan: ");
+        String namaOrang = sc.nextLine();
+        sc.close();
+        return namaOrang;
+    }
+
+    public static void ucapanTerimaKasih()
+    {
+        String nama =  PenerimaUcapan();
+        System.out.println("Thank you "+ nama +" for being the best teacher in the world.\n"+
+        "You inspired in me a love  for learning and made me feel like i could ask you anything.");
+    }
+
+    public static void main(String[] args) {
+        ucapanTerimaKasih();
+    }
+}
