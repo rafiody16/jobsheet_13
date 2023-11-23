08f738c5ca0893fb0ddd7a63694323ad70f321e1
rafiody16 <rafiody16@gmail.com>
2023-11-23 20:40:55 +0700

    Percobaan 2

--- TerimaKasih22.java
+++ TerimaKasih22.java
@@ -8,7 +8,14 @@ public class TerimaKasih22 {
         "You inspired in me a love  for learning and made me feel like i could ask you anything.");
     }
 
+    public static void ucapanTambahan (String ucapan)
+    {
+        System.out.println(ucapan);
+    }
+
     public static void main(String[] args) {
         ucapanTerimaKasih();
+        String ucapan = "Terima Kasih Pak.. Bu.. Sehat Selalu";
+        ucapanTambahan(ucapan);
     }
 }
